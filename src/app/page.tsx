f28e180d45b5cb566c58fad9f074a70d47a2679b
devSaifur <saifur2akash@gmail.com>
2024-01-30 02:12:04 +0600

    display product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MaxWidthWrapper from '../components/MaxWidthWrapper'
 import Link from 'next/link'
 import { Button, buttonVariants } from '@/components/ui/button'
 import { ArrowDownToLine, CheckCircle, Leaf } from 'lucide-react'
+import ProductReel from '@/components/ProductReel'
 
 const perks = [
   {
@@ -44,9 +45,13 @@ export default function Home() {
             <Button variant="ghost">Our quality promise &rarr;</Button>
           </div>
         </div>
-      </MaxWidthWrapper>
 
-      {/* todo: products */}
+        <ProductReel
+          query={{ sort: 'desc', limit: 4 }}
+          title="brand new"
+          href="product collection link"
+        />
+      </MaxWidthWrapper>
 
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
